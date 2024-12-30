f4c59ff418835e8421fa18bd722992d3e3387304
cbikram82 <bikram82@gmail.com>
2024-12-30 15:54:48 +0000

    Initial Commit

---
+++ AppTest/AppTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppTestApp.swift
+//  AppTest
+//
+//  Created by Bikram Chatterjee on 30/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct AppTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppTestTests/AppTestTests.swift
@@ -0,0 +1,17 @@
+//
+//  AppTestTests.swift
+//  AppTestTests
+//
+//  Created by Bikram Chatterjee on 30/12/2024.
+//
+
+import Testing
+@testable import AppTest
+
+struct AppTestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
